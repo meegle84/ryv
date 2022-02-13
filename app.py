6fb26d891847452a73d8c704acb0ef909a54b25b
meegle84 <ckn.niwatori@gmail.com>
2022-02-13 20:05:55 +0100

    Add: firs embedded youtube video

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template
+
+# Gandalf Sax Guy 10 Hours HD
+DEFAULT_ID = 'G1IbRujko-A'
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<p>Hello, World!</p>"
+# define homage
+@app.route('/', methods=['GET'])
+def homepage():
+    return render_template('index.html', youtube_id=DEFAULT_ID)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
